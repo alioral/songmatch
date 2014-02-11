725b439103d13d36c74bc8a6cf105c11f9a5396a
Ali Oral <alioral@Ali-MacBook-Pro.local>
2014-02-11 22:57:44 +0200

    fix

--- smapp/database.py
+++ smapp/database.py
@@ -9,7 +9,7 @@ class Database(object):
 			db_string, db_name = 'mongodb://localhost:27017/', 'songmatch'
 			if os.environ.get('MONGOLAB_URI'):
 				db_string = os.environ.get('MONGOLAB_URI')
-				db_name = 'songmatch'
+				db_name = 'heroku_app22127074'
 			client = MongoClient(db_string)
 			cls._database = client[db_name]
 		return cls._database
